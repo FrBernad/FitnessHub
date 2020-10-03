c57eb96e904f617c1412d03189623853ee05e17c
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2020-10-03 18:17:22 -0300

    cambios en homePage

--- fitness-hub-app/src/views/UserHome.vue
+++ fitness-hub-app/src/views/UserHome.vue
@@ -1,7 +1,28 @@
 <template>
-  <v-container fluid fill-height class="bg">
-  </v-container>
 
+  <v-container fluid fill-height class="pa-0">
+    <v-row justify="center" align="center" class="fill-height">
+        <v-col cols="4" class="fill-height pa-0">
+          <v-card rounded="0" height="100%" class="workoutsBG iluminate d-flex align-center justify-center">
+            <v-card-title class="font-weight-bold text-h3 white--text">WORKOUTS</v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="4" class="fill-height pa-0">
+          <v-card rounded="0" height="100%" class="createRoutineBG iluminate d-flex align-center justify-center">
+            <v-card-title class="font-weight-bold text-h3 white--text">CREATE ROUTINE</v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="4" class="fill-height pa-0">
+          <v-card rounded="0" height="100%" class="myRoutineBG iluminate d-flex align-center justify-center">
+            <v-card-title class="font-weight-bold text-h3 white--text">MY ROUTINES</v-card-title>
+
+          </v-card>
+
+
+        </v-col>
+    </v-row>
+
+  </v-container>
 </template>
 
 <script>
@@ -11,12 +32,31 @@
 </script>
 
 <style scoped>
-  .bg{
-    height: 100%;
-    background: linear-gradient(
-      rgba(33, 37, 41, 0.4),
-      rgba(33, 37, 41, 0.4)
-    ), url("../assets/imgs/Home.jpg") no-repeat center;
-    background-size: cover;
+  .createRoutineBG{
+      height: 100%;
+      background: url("../assets/imgs/createRoutinesImg.jpg") no-repeat center;
+      background-size: cover;
+      opacity: 1;
   }
+
+  .workoutsBG{
+    height: 100%;
+    background: url("../assets/imgs/WorkoutsHome.png") no-repeat center;
+    background-size: cover;
+    opacity: 1;
+  }
+
+  .myRoutineBG{
+    height: 100%;
+    background: url("../assets/imgs/myRoutinesHome.jpg") no-repeat center;
+    background-size: cover;
+    opacity: 1;
+  }
+  .iluminate{
+    transition: opacity .3s ease-in-out ;
+  }
+  .iluminate:hover{
+    opacity: 0.8;
+  }
+
 </style>
