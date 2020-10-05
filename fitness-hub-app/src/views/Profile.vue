2fe31adf82a67061e109c357b2b63e4133da4846
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2020-10-04 23:13:07 -0300

    arreglo del frano

--- fitness-hub-app/src/views/Profile.vue
+++ fitness-hub-app/src/views/Profile.vue
@@ -4,21 +4,27 @@
       <v-col justify="center" cols="8">
         <v-card class="fill-height" color="rgb(233, 236, 239)" dark>
           <v-img height="25%" src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"></v-img>
-          <v-col style="position:relative">
+          <v-row>
+          <v-col cols="12" style="position:relative">
             <v-avatar size="180" style="position:absolute; top: -100px" class="ml-6 elevation-5">
               <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
             </v-avatar>
           </v-col>
-          <v-col justify="center" align="left" offset-md="3">
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title class="black--text">Marcus Obrien</v-list-item-title>
-                <v-list-item-title class="black--text">Age: 20</v-list-item-title>
-                <v-list-item-title class="black--text">Country: Argentina</v-list-item-title>
-
-              </v-list-item-content>
-            </v-list-item>
+          <v-col cols="12">
+            <v-row>
+              <v-col cols="4" class="offset-3">
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title class="black--text">Marcus Obrien</v-list-item-title>
+                    <v-list-item-title class="black--text">Age: 20</v-list-item-title>
+                    <v-list-item-title class="black--text">Country: Argentina</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-col>
+            </v-row>
           </v-col>
+          </v-row>
+
           <v-col justify="center" align="left">
             <v-container fluid>
               <v-textarea
@@ -44,7 +50,7 @@ export default {
   name: "Profile",
   data: () => ({
     rules: [v => v.length <= 100 || 'Max 100 characters'],
-    value:'Eras assimilant in tolosa! Core, mystery, and vision. All children like shreded chocolates in orange juice and garlic. Dimension is not one in chaos, the kingdom, or private places, but everywhere..' ,
+    value: 'Eras assimilant in tolosa! Core, mystery, and vision. All children like shreded chocolates in orange juice and garlic. Dimension is not one in chaos, the kingdom, or private places, but everywhere..',
   }),
 }
 </script>
