80a50a8889fdfbd7c4604e3ccaf2d13dd3f93bac
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2020-10-16 14:57:43 -0300

    agrego boton en rutina

--- fitness-hub-app/src/components/Exercise.vue
+++ fitness-hub-app/src/components/Exercise.vue
@@ -1,30 +1,36 @@
 <template>
 
-<v-dialog v-model="dialog" width="500px">
+<v-dialog v-model="dialog" persistent width="500px">
 
 <template v-slot:activator="{ on, attrs }">
-  <v-list-item class="elevation-1 listItem" v-bind="attrs" v-on="on">
+  <v-list-item  class="elevation-1 listItem">
     <v-list-item-content class="justify-center">
         <v-row>
-          <v-col cols="8" class="d-flex align-center justify-start"><h3>Exercise 1</h3></v-col>
-          <v-col cols="4" class="d-flex align-center justify-end"><h3>15</h3></v-col>
+          <v-col cols="2" class="d-flex align-center justify-start"><h3>15</h3></v-col>
+          <v-col cols="8" class="d-flex align-center justify-center"><h3>Exercise 1</h3></v-col>
+          <v-col cols="2" class="d-flex align-center justify-end"><v-btn v-bind="attrs" v-on="on" icon large><v-icon>mdi-information</v-icon></v-btn></v-col>
         </v-row>
     </v-list-item-content>
   </v-list-item>
 </template>
 
   <v-card>
-    <v-card-title class="text-h4 font-weight-bold d-flex justify-center text-decoration-underline">Exercise 1</v-card-title>
+    <v-row class="">
+      <v-col cols="12" class="d-flex ">
+        <v-btn @click="dialog = false"><v-icon>mdi-information</v-icon></v-btn>
+      </v-col>
+    </v-row>
+    <v-card-title class="text-h4 font-weight-bold d-flex justify-center">Exercise 1</v-card-title>
     <v-container>
-      <v-row>
-        <v-col cols="6" ><v-card-text class="text-h5 font-weight-bold d-flex align-center justify-center pa-0 ">PICTURE</v-card-text></v-col>
-        <v-col cols="6" ><v-card-text class="text-h5 font-weight-bold d-flex align-center justify-center pa-0">VIDEO</v-card-text></v-col>
+      <v-row class="align-center justify-center">
+        <v-col cols="6"><v-card-text class="text-h5  d-flex align-center justify-center font-weight-bold pa-0 ">PICTURE</v-card-text></v-col>
+        <v-col cols="6"><v-card-text class="text-h5  d-flex align-center justify-center font-weight-bold pa-0">VIDEO</v-card-text></v-col>
       </v-row>
-      <v-row>
-        <v-col cols="6"><v-img src="../assets/imgs/routines.jpg" max-height="100" max-width="100"></v-img></v-col>
-        <v-col cols="6"><v-img src="../assets/imgs/routines.jpg" max-height="100" max-width="100"></v-img></v-col>
+      <v-row class="align-center justify-center">
+        <v-col cols="6" class="d-flex align-center justify-center"><v-img src="../assets/imgs/routines.jpg" max-height="100" max-width="100"></v-img></v-col>
+        <v-col cols="6" class="d-flex align-center justify-center"><v-img src="../assets/imgs/routines.jpg" max-height="100" max-width="100"></v-img></v-col>
       </v-row>
-      <v-row>
+      <v-row class="align-center justify-center">
         <v-col cols="12" class="text-center">
           <v-card-text class="text-h5 font-weight-bold d-flex align-center justify-center pa-0">
             DESCRIPTION
@@ -49,6 +55,7 @@ export default {
 name: "Exercise",
   data () {
     return {
+      title:'',
       dialog: false,
     }
   },
