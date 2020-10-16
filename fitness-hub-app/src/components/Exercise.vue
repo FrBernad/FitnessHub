b16f70fdcd37cd8198dd372fac03461d398669d7
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2020-10-16 13:45:41 -0300

    Exercise overlay

--- fitness-hub-app/src/components/Exercise.vue
+++ fitness-hub-app/src/components/Exercise.vue
@@ -14,7 +14,30 @@
 </template>
 
   <v-card>
-    <v-card-title>PRIVACY</v-card-title>
+    <v-card-title class="text-h4 font-weight-bold d-flex justify-center text-decoration-underline">Exercise 1</v-card-title>
+    <v-container>
+      <v-row>
+        <v-col cols="6" ><v-card-text class="text-h5 font-weight-bold d-flex align-center justify-center pa-0 ">PICTURE</v-card-text></v-col>
+        <v-col cols="6" ><v-card-text class="text-h5 font-weight-bold d-flex align-center justify-center pa-0">VIDEO</v-card-text></v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6"><v-img src="../assets/imgs/routines.jpg" max-height="100" max-width="100"></v-img></v-col>
+        <v-col cols="6"><v-img src="../assets/imgs/routines.jpg" max-height="100" max-width="100"></v-img></v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" class="text-center">
+          <v-card-text class="text-h5 font-weight-bold d-flex align-center justify-center pa-0">
+            DESCRIPTION
+          </v-card-text>
+          <v-card-text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</v-card-text>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+        <v-card-text class="text-h5 font-weight-bold d-flex align-center justify-center pa-0">REPS/TIME: 15</v-card-text>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 
 </v-dialog>
