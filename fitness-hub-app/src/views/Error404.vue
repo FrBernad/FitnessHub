5ce90972080d25bed814337c0dc39a03e167b86c
Francisco <frbernad@itba.edu.ar>
2020-10-22 19:27:12 -0300

    changes on page not found view

--- fitness-hub-app/src/views/Error404.vue
+++ fitness-hub-app/src/views/Error404.vue
@@ -1,9 +1,13 @@
 <template>
   <v-container fill-height fluid>
-    <v-row align="center">
-      <v-col col="12">
-        <h1 class="text-center text-h3 font-weight-bold mb-5">Ooops... Error 404</h1>
-        <h2 class="text-center text-h4 font-weight-medium">Lo sentimos, pero la página que busca no existe.</h2>
+    <v-row align="center" justify="center">
+      <v-col cols="11" md="8">
+        <v-card light color="F8F9FA" class="pa-5" elevation="5">
+          <v-img src="../assets/imgs/LongBrandBlack.png"
+                 :max-height="this.$vuetify.breakpoint.smAndDown ? '120':'180'" contain/>
+          <h1 class="text-center text-h5 text-md-h3 font-weight-bold mb-5">Ooops... Error 404</h1>
+          <h2 class="text-center text-h6 text-md-h4 font-weight-medium my-5">The requested url, was not found on this server.</h2>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
